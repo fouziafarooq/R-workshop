46e39a9387fb6e4186c65a5182dd464462b00d81
Fouzia Farooq <fof3@pitt.edu>
2024-06-07 13:54:18 -0400

    updated code.

--- New-Dataframe.R
+++ New-Dataframe.R
@@ -1,12 +1,17 @@
-df <- data.frame(momid = c("IN-111", "IN-123", "PK-222", "KY-333", "ZM-444", "GH-555"),
-                 age = c(22, 23, 22, 25, 34, 22),
-                 weight = c(75.2, 65.9, 62.0, 78.5, 80.5, 55.7),
-                 smoke = c(TRUE, FALSE, FALSE, TRUE, FALSE, FALSE))
+df <- data.frame(momid = c("IN-111", "IN-123", "PK-222", "KY-333", "ZM-444", "GH-555", "PK-222"),
+                 pregid = c(101, 102, 103, 104, 105, 106, 107),
+                 age = c(22, 23, 22, 25, 34, 22, 24),
+                 weight = c(75.2, 65.9, 62.0, 78.5, 80.5, 55.7, 69.0),
+                 smoke = c(TRUE, FALSE, FALSE, TRUE, FALSE, FALSE, FALSE))
 
 write.csv(df, 'data/PRISMA-dummy-data.csv', row.names = FALSE)
 
-df2 <- read.csv('https://go.gwu.edu/rprismadummydata')
+dir.create('data')
 
+download.file('https://go.gwu.edu/rprismadummydata', "data/PRISMA-dummy-data.csv")
+df <- read.csv('data/PRISMA-dummy-data.csv')
+
+#################
 
 df <- data.frame(momid = c("IN-234", "IN-343", "PK-232", "KY-656", "ZM-532", "GH-948"),
                  age = c(22, 23, 22, 25, 34, 22),
